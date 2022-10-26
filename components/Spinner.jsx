6e0fea7d4b93ce6cfa3297b8a2244c95d054b7af
njoro410 <brian.njoroge@student.moringaschool.com>
2022-10-26 13:13:05 +0300

    added spinner component

---
+++ components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import spinner from "../public/loading.gif";
+
+const Spinner = () => {
+  return (
+    <div>
+      <Image className="w-[200px] m-auto block" alt="loading..." src={spinner} />
+    </div>
+  );
+};
+
+export default Spinner;
